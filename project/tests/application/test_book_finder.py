7bb356c91baba2affed810198e42d561cb65a0ce
niconunez96 <nicolas110996@gmail.com>
2020-12-05 12:02:52 -0300

    Adding unit tests for book finder

--- project/tests/application/test_book_finder.py
+++ project/tests/application/test_book_finder.py
@@ -1,7 +1,9 @@
+import pytest
 from mock import Mock
 
 from books.domain.book import Book
 from books.application.books_finder import BookFinder, BooksFinder
+from books.domain.book_repository import EntityNotFound
 
 
 class TestBookFinder:
@@ -18,14 +20,50 @@ class TestBookFinder:
             'id': None,
             'title': "title",
             'description': "desc",
-            'created': None,
+            'created': 'None',
         } == book_found
 
     def test_should_return_entity_not_found_when_id_does_not_exist(self):
-        pass
+        stub_repository = Mock(
+            find_by_id=Mock(side_effect=EntityNotFound)
+        )
+        book_finder = BookFinder(book_repository=stub_repository)
+
+        with pytest.raises(EntityNotFound):
+            book_finder.execute(book_id=1)
 
 
 class TestBooksFinder:
 
     def test_should_return_books_found_by_repository(self):
-        pass
+        stub_repository = Mock(
+            find_all=Mock(return_value=[
+                Book("title1", "desc"),
+                Book("title2", "desc"),
+                Book("title3", "desc"),
+            ])
+        )
+        books_finder = BooksFinder(book_repository=stub_repository)
+
+        books_found = books_finder.execute()
+
+        assert [
+            {
+                'id': None,
+                'title': "title1",
+                'description': "desc",
+                'created': 'None',
+            },
+            {
+                'id': None,
+                'title': "title2",
+                'description': "desc",
+                'created': 'None',
+            },
+            {
+                'id': None,
+                'title': "title3",
+                'description': "desc",
+                'created': 'None',
+            },
+        ] == books_found
